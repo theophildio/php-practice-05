17df57c9753f39c060b9c174aa62720c779dc005
Theophil Dio <dtheo.sangma@gmail.com>
2023-10-25 11:21:20 +0600

    Our Crew Project added to repo

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crew ProjectS</title>
+</head>
+<body>
+    <h1>Welcome to <span>our crew project</span></h1>
+</body>
+</html>
